ff32242a539394e7406999dd06ae2215c8ebb1f6
Hendrik <dlcapital.dev@gmail.com>
2026-02-22 11:43:19 +0200

    Added functions for claim_id and policy_id fixes

--- clean_data/business_claims_clean.py
+++ clean_data/business_claims_clean.py
@@ -1,8 +1,8 @@
 from data_store import messy_datasets
 import pandas as pd
 pd.set_option("display.max_columns", None)
-import re
-from missing_policy_fix import missing_id_fix, validate_ids
+from id_fix import IDCleaner
+from fill_missing_policy_ids import fill_missing_policy_ids
 """
 DATASET DOCUMENTATION: Business Interruption Claims
 ==================================================
@@ -11,7 +11,8 @@ Variable Name       | Description                          | Type      | Units  
 policy_id           | Policy identifier                   | character | --          | e.g., BI-123456, ...
 station_id          | Mining platform identifier          | category  | --          | e.g., A1, G3, ...
 solar_system        | Solar system identifier             | category  | --          | {Helionis Cluster, Epsilon, Zeta}
-production_load     | Avg utilization rate of capacity    | numeric   | Ratio       | 0 – 1
+production_load     | Avg utilizati
+on rate of capacity    | numeric   | Ratio       | 0 – 1
 energy_backup_score | Adequacy of emergency power systems | category  | --          | {1, 2, 3, 4, 5}
 supply_chain_index  | Proportion of inputs sourced        | numeric   | Ratio       | 0 – 1
 avg_crew_exp        | Average experience of assigned staff| numeric   | Earth years | 1 – 30
@@ -26,13 +27,23 @@ business_claims_freq = messy_datasets["business_claims_freq"]
 business_claims_sev  = messy_datasets["business_claims_sev"]
 print("Datasets loaded correctly!")
 
-business_claims_freq["policy_id"] = missing_id_fix(business_claims_freq["policy_id"], 9)
-summary = validate_ids(business_claims_freq["policy_id"], 9)
-print(summary)
 
-result = validate_ids(business_claims_freq["policy_id"], 9)
+business_claims_freq["policy_id"] = IDCleaner(business_claims_freq["policy_id"], 9).broken_id_fix().missing_id_fix().get()
+print(f"{IDCleaner(business_claims_freq['policy_id'], 9).validate_ids()} in the business_claims_freq policy_id column!")
 
-if result != "No issues found":
-    indices = [i for i, _ in result]
-    non_zero = business_claims_freq.loc[indices, "claim_count"] != 0
-    print(business_claims_freq.loc[indices][non_zero.values])
+business_claims_sev["claim_id"] = IDCleaner(business_claims_sev["claim_id"], 12).broken_id_fix().missing_id_fix().get()
+print(f"{IDCleaner(business_claims_sev['claim_id'], 12).validate_ids()} in the business_claims_sev claim_id column!")
+
+business_claims_sev = fill_missing_policy_ids(business_claims_sev, business_claims_freq)
+business_claims_sev["policy_id"] = IDCleaner(business_claims_sev["claim_id"], 9).broken_id_fix().get()
+print(f"{IDCleaner(business_claims_sev['policy_id'], 9).validate_ids()} in the business_claims_sev policy_id column!")
+
+"""station_id          | Mining platform identifier          | category  | --          | e.g., A1, G3, ...
+solar_system        | Solar system identifier             | category  | --          | {Helionis Cluster, Epsilon, Zeta}
+energy_backup_score | Adequacy of emergency power systems | category  | --          | {1, 2, 3, 4, 5}
+supply_chain_index  | Proportion of inputs sourced        | numeric   | Ratio       | 0 – 1
+avg_crew_exp        | Average experience of assigned staff| numeric   | Earth years | 1 – 30
+maintenance_freq    | Expected maintenance cycles / year  | numeric   | Number      | 0 – 6
+safety_compliance   | Compliance score                    | category  | --          | {1, 2, 3, 4, 5}
+exposure            | Period of exposure in Earth years   | numeric   | Ratio       | 0 – 1
+claim_count         | Number of claims during exposure    | numeric   | Number      | 0 – 4"""
